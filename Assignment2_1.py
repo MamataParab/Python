d51f30611c149d3cc1864af696db8a29ed9af5bc
MamataParab <45738125+MamataParab@users.noreply.github.com>
2019-03-14 15:49:23 +0530

    Add files via upload

---
+++ Assignment2_1.py
@@ -0,0 +1,11 @@
+def Pattern1(No):
+    for i in range(0,No):
+        for j in range(0,No):
+            print("*", end=" ");
+        print("\n");  
+
+
+print("Enter Number");
+x=int(input());
+Pattern1(x);
+            
